e27661e33cf7ed378156aa4fa252c3d41ab84f48
ankit <ankit@ankits-MacBook-Air.local>
2013-09-29 01:30:02 +0800

    changing sender name to admin

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationMailer < ActionMailer::Base
   default :from => "#{Rails.application.name} " <<
-    "<nobody@#{Rails.application.domain}>"
+    "<admin@#{Rails.application.domain}>"
 
   def invitation(invitation)
     @invitation = invitation
