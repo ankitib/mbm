eabefd00c10d514922ffca2f07c8237b6e9bf33c
ankit <ankit@ankits-MacBook-Air.local>
2013-09-29 01:00:37 +0800

    adding email server info

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  :address => "127.0.0.1",
-  :port => 25,
-  :domain => Rails.application.domain,
-  :enable_starttls_auto => false,
+  :authentication => :plain,
+  :address => "smtp.mailgun.org",
+  :port => 587,
+  :domain => "app18375044.mailgun.org",
+  :user_name => "postmaster@app18375044.mailgun.org",
+  :password => "4lzt9j50jxq2"
 }
